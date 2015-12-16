972d8f062cf5176b0f066b5eef2215a9c4e3df1f
N <nielis_brouwer@hotmail.com>
2015-12-16 14:40:03 +0100

    [FIXED] added php docs

--- src/Slack.php
+++ src/Slack.php
@@ -5,8 +5,19 @@ namespace CJSDevelopment;
 use GuzzleHttp\Client;
 use GuzzleHttp\Exception\RequestException;
 
+/**
+ * Class Slack
+ * @package CJSDevelopment
+ */
 class Slack
 {
+    /**
+     * @param $message
+     * @param null $channel = chanel where the message get's send to
+     * @param null $username = under what username the message get's send to
+     * @param null $icon = the icon that is displayed by the message
+     * @return bool
+     */
     public static function sendMessage($message, $channel = null, $username = null, $icon = null) {
         $data = self::getPayload($message, $channel, $username, $icon);
 
@@ -20,6 +31,13 @@ class Slack
         return true;
     }
 
+    /**
+     * @param $message
+     * @param null $channel = chanel where the message get's send to
+     * @param null $username = under what username the message get's send to
+     * @param null $icon = the icon that is displayed by the message
+     * @return mixed
+     */
     private static function getPayload($message, $channel = null, $username = null, $icon = null) {
         $object["channel"]      = is_null($channel) ? config("slack.channel") : $channel;
         $object["username"]     = is_null($username) ? config("slack.username") : $username;
